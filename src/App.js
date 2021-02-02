cd7327f30321c4d7cbd47c57cca98829a9b717f3
mbrabenec <brabzzz@gmail.com>
2021-02-02 22:43:47 +0100

    ul

--- src/App.js
+++ src/App.js
@@ -1,29 +1,128 @@
+// import React, { useState } from "react";
+// import { Seat } from "./Seat.js";
+
+// function App() {
+//   const [nums, setNums] = useState([
+//     1,
+//     2,
+//     3,
+//     4,
+//     5,
+//     6,
+//     7,
+//     8,
+//     9,
+//     10,
+//     11,
+//     12,
+//     13,
+//     14,
+//     15,
+//     16,
+//   ]);
+
+//   return (
+//     <div className="carriage-row">
+//       {nums.map((value, index) => {
+//         return <Seat seatNum={value} key={index} />;
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
-import { Seat } from './Seat.js';
+import "./index.css";
+import { Seat } from "./Seat";
 
 function App() {
+  const numberOfSeats = 48;
 
-  // function handleClick(e) {
-  //   console.log(e);
-  // }
-  
-const [nums, setNums] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+  const [tickets, setTickets] = useState(1);
+  const [seats, setSeats] = useState(
+    new Array(numberOfSeats)
+      .fill("free")
+      .map((s) => (Math.random() < 0.5 ? s : "occupied"))
+  );
 
+  const handleSeatClick = (seat) => {
+    if (seats[seat] === "free") {
+      // count how many items in our seats array has status "selected" //MB MOD - can peel back to single //
+      // const selectedSeats = seats.reduce((a, c) => {
+      //   if (c === "selected") return a + 1;
+      //   return a;
+      // }, 0);
+
+      // // if allready selected all tickets return from this method
+      // if (selectedSeats >= tickets) return;
+
+      // modify seat with index `seat` to status "selected"
+      setSeats(
+        seats.map((status, i) => {
+          if (i === seat) return "selected";
+          return status;
+        })
+      );
+    } else if (seats[seat] === "selected") {
+      // modify seat with index `seat` to status "free"
+      setSeats(
+        seats.map((status, i) => {
+          if (i === seat) return "free";
+          return status;
+        })
+      );
+    }
+  };
 
   return (
-    <div className="carriage-row">
+    <div className="App">
 
-    {
-    nums.map((value, index) => {
 
-        return < Seat seatNum={value} key={index}/>
-      })
-      
-      }
+      <div>
+        <label>
+          How many tickets do you want?
+          <select value={tickets} onChange={(e) => setTickets(e.target.value)}>
+            {new Array(10).fill(null).map((n, i) => (
+              <option key={i + 1} value={i + 1}>
+                {i + 1}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+
+
+      <div className="carriage">
+        {new Array(numberOfSeats / 4).fill(null).map((n, row) => (
+
+          <div key={row} className="carriage-row">
+
+            <div onClick={() => handleSeatClick(row * 4 + 0)}>
+              <Seat number={row * 4 + 1} status={seats[row * 4 + 0]} />
+            </div>
+
+            <div onClick={() => handleSeatClick(row * 4 + 1)}>
+              <Seat number={row * 4 + 2} status={seats[row * 4 + 1]} />
+            </div>
+
+            <div className="aisle-spacer"></div>
+
+            <div onClick={() => handleSeatClick(row * 4 + 2)}>
+              <Seat number={row * 4 + 3} status={seats[row * 4 + 2]} />
+            </div>
+            <div onClick={() => handleSeatClick(row * 4 + 3)}>
+              <Seat number={row * 4 + 4} status={seats[row * 4 + 3]} />
+            </div>
+
+          </div>
+
+        ))}
+      </div>
+
 
     </div>
   );
-
-  }
+}
 
 export default App;
--- src/Seat.js
+++ src/Seat.js
@@ -1,36 +1,61 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-export function Seat(props) {
+// export function Seat(props) {
+//   let [seatClass, setSeatClass] = useState("free");
 
-    let [seatClass, setSeatClass] = useState("free");
+//   function handleClick(e) {
+//     if (seatClass === "free") {
+//       setSeatClass("selected");
+//     } else {
+//       setSeatClass("free");
+//     }
+//   }
 
-    
+//   return (
+//     <svg
+//       key={props.seatNum}
+//       onClick={handleClick}
+//       className="train-seat"
+//       width="100"
+//       height="100"
+//       viewBox="0 0 100 100"
+//     >
+//       <g className={`train-seat__seat train-seat__seat--${seatClass}`}>
+//         <rect x="10" y="15" width="80" height="70" rx="15" ry="15" />
+//         <rect x="30" y="10" width="52" height="15" rx="3" ry="3" />
+//         <rect x="30" y="75" width="52" height="15" rx="3" ry="3" />
+//         <rect x="70" y="10" width="20" height="80" rx="9.821" ry="10" />
+//       </g>
 
-    function handleClick(e) {
-        if(seatClass === "free") {
-            setSeatClass("selected")
-        } else {setSeatClass("free")};
-    }
+//       <text className="train-seat__number" x="25" y="60">
+//         {props.seatNum}
+//       </text>
+//     </svg>
+//   );
+// }
+
+import React from 'react';
 
 
+export function Seat({number, status}) {
+    return (
+        <svg
+            className="train-seat"
+            width="100"
+            height="100"
+            viewBox="0 0 100 100"
+        >
+            <g className={`train-seat__seat train-seat__seat--${status}`}>
+                <rect x="10" y="15" width="80" height="70" rx="15" ry="15"/>
+                <rect x="30" y="10" width="52" height="15" rx="3" ry="3"/>
+                <rect x="30" y="75" width="52" height="15" rx="3" ry="3"/>
+                <rect x="70" y="10" width="20" height="80" rx="9.821" ry="10"/>
+            </g>
 
-  return (
+            <text className="train-seat__number" x="25" y="60">
+                {number}
+            </text>
+        </svg>
+    )
+} 
 
-    <svg 
-    key={props.seatNum}
-    onClick={handleClick}
-    className="train-seat" width="100" height="100" viewBox="0 0 100 100">
-      <g className={`train-seat__seat train-seat__seat--${seatClass}`}>
-        <rect x="10" y="15" width="80" height="70" rx="15" ry="15" />
-        <rect x="30" y="10" width="52" height="15" rx="3" ry="3" />
-        <rect x="30" y="75" width="52" height="15" rx="3" ry="3" />
-        <rect x="70" y="10" width="20" height="80" rx="9.821" ry="10" />
-      </g>
-
-      <text className="train-seat__number" x="25" y="60">
-        {props.seatNum}
-      </text>
-    </svg>
-
-  );
-}
